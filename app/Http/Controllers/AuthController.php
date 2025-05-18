cf4ad6af826b875adf3477b3e0d2625f662a2e6e
Khaled-Alabadla <kh.es.abadla@gmail.com>
2025-05-18 15:52:03 +0300

    new commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,11 +31,11 @@ class AuthController extends Controller
             }
 
             if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-                return redirect()->route('dashboard');  // Login success
+                return redirect('https://sim-academy.vercel.app/home');  // Login success
             } else {
                 return response()->json(
                     ['errors' => $validate->errors()]
-                ); // Login failed (wrong credentials)
+                );
             }
         } catch (\Exception $e) {
             // Return the actual error message for debugging
